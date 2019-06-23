e22ec4b1cfe887ec314b43d47fbb619ae5ed61eb
AKwoKWH <AKwoKWH+Github@gmail.com>
2019-06-23 20:57:45 +0800

    [Requesting Null.jpg Error] bypass

--- instagram_scraper/app.py
+++ instagram_scraper/app.py
@@ -156,6 +156,11 @@ class InstagramScraper(object):
         time.sleep(secs % min_delay)
 
     def _retry_prompt(self, url, exception_message):
+#         Temp bypass for requesting null.jpg ERROR.
+        if "null" in str(url):
+            self.logger.info( 'This is a [Requesting Null.jpg Error] - Auto Abort' )
+            return None
+        
         """Show prompt and return True: retry, False: ignore, None: abort"""
         answer = input( 'Repeated error {0}\n(A)bort, (I)gnore, (R)etry or retry (F)orever?'.format(exception_message) )
         if answer:
